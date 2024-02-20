b20db8a1bdc44da94c9c57a206bcc8fffd4817cc
Dias <raibekovdias@gmail.com>
2024-02-20 20:35:29 +0600

    Angular

---
+++ lab4/store_app/src/app/products.ts
@@ -0,0 +1,108 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  img: string;
+  rating: string;
+  link: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: "Google Pixel 8 Pro",
+    price: 549900,
+    description: "12 ГБ/256 ГБ синий",
+    img: "https://resources.cdn-kaspi.kz/img/m/p/h13/h9c/84328657780766.jpg?format=gallery-medium",
+    rating: "5",
+    link: "https://kaspi.kz/shop/p/google-pixel-8-pro-12-gb-256-gb-sinii-114016769/?c=750000000",
+  },
+  {
+    id: 2,
+    name: "Apple iPhone 12",
+    price: 260117,
+    description: "64Gb синий",
+    img: "https://resources.cdn-kaspi.kz/img/m/p/ha9/h53/64083673776158.jpg?format=gallery-medium",
+    rating: "5",
+    link: "https://kaspi.kz/shop/p/apple-iphone-12-64gb-sinii-100656842/?c=750000000",
+  },
+  {
+    id: 3,
+    name: "Apple iPhone 13",
+    price: 288597,
+    description: "128Gb зеленый",
+    img: "https://resources.cdn-kaspi.kz/img/m/p/hd1/h2f/64255724159006.jpg?format=gallery-medium",
+    rating: "5",
+    link: "https://kaspi.kz/shop/p/apple-iphone-13-128gb-zelenyi-104078887/?c=750000000",
+  },
+  {
+    id: 4,
+    name: "Apple iPhone 14",
+    price: 324993,
+    description: "128Gb голубой",
+    img: "https://resources.cdn-kaspi.kz/img/m/p/hbc/h02/64481569701918.jpg?format=gallery-medium",
+    rating: "5",
+    link: "https://kaspi.kz/shop/p/apple-iphone-14-128gb-goluboi-106363150/?c=750000000",
+  },
+  {
+    id: 5,
+    name: "Apple iPhone 15 Pro Max",
+    price: 612783,
+    description: "256Gb черный",
+    img: "https://resources.cdn-kaspi.kz/img/m/p/hdf/hc7/83559837007902.jpg?format=gallery-medium",
+    rating: "5",
+    link: "https://kaspi.kz/shop/p/samsung-galaxy-s22-ultra-12-gb-256-gb-zelenyi-103671158/?c=750000000",
+  },
+  {
+    id: 6,
+    name: "Samsung Galaxy S22 Ultra",
+    price: 446646,
+    description: "12 ГБ/256 ГБ зеленый",
+    img: "https://resources.cdn-kaspi.kz/img/m/p/h0f/h29/64376370135070.jpg?format=gallery-medium",
+    rating: "5",
+    link: "https://kaspi.kz/shop/p/samsung-galaxy-s22-ultra-12-gb-256-gb-zelenyi-103671158/?c=750000000",
+  },
+  {
+    id: 7,
+    name: "Samsung Galaxy S23 Ultra",
+    price: 442773,
+    description: "12 ГБ/256 ГБ зеленый",
+    img: "https://resources.cdn-kaspi.kz/img/m/p/hb0/hf3/69635680239646.jpg?format=gallery-medium",
+    rating: "5",
+    link: "https://kaspi.kz/shop/p/samsung-galaxy-s23-ultra-12-gb-256-gb-zelenyi-109174565/?c=750000000",
+  },
+  {
+    id: 8,
+    name: "Samsung Galaxy S24 Ultra 5G",
+    price: 579291,
+    description: "12 ГБ/256 ГБ черный",
+    img: "https://resources.cdn-kaspi.kz/img/m/p/hce/h74/84963707191326.png?format=gallery-medium",
+    rating: "5",
+    link: "https://kaspi.kz/shop/p/samsung-galaxy-s24-ultra-5g-12-gb-256-gb-chernyi-116044354/?c=750000000",
+  },
+  {
+    id: 9,
+    name: "OPPO Find N3",
+    price: 995000,
+    description: "16 ГБ/512 ГБ черный",
+    img: "https://resources.cdn-kaspi.kz/img/m/p/hcd/h9e/84634276364318.jpg?format=gallery-medium",
+    rating: "5",
+    link: "https://kaspi.kz/shop/p/oppo-find-n3-16-gb-512-gb-chernyi-115053402/?c=750000000",
+  },
+  {
+    id: 10,
+    name: "OPPO Reno11 5G",
+    price: 289999,
+    description: "12 ГБ/256 ГБ серый",
+    img: "https://resources.cdn-kaspi.kz/img/m/p/h93/ha8/84982452027422.jpg?format=gallery-medium",
+    rating: "",
+    link: "https://kaspi.kz/shop/p/oppo-reno11-5g-12-gb-256-gb-seryi-116101268/?c=750000000",
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
